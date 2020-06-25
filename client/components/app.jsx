734b089a9318231cc51cb7299d91cf900f052579
Mayanne Chess <mayplusmay@gmail.com>
2020-06-25 13:47:38 -0400

    Set up directory structure

---
+++ client/components/app.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    state = {
+      hello: true
+    };
+
+  }
+
+  render() {
+    return(
+      <div>Hello world</div>
+    );
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
